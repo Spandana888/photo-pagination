a208fb4693d48a6da385b60f4c1818b0b4b40b18
Spandana Chinnappa <56736951+Spandana888@users.noreply.github.com>
2023-12-28 08:52:53 +0530

    pagination changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  const itemsPerPage = 100;
+  const allItems = [...data];
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = allItems.slice(indexOfFirstItem, indexOfLastItem);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Color Picker</h1>
+      {currentItems.map((list) => {
+        return <img src={list.url} alt={list.title} key={list.id} />;
+      })}
+      <br />
+      {/* Pagination Button */}
+      {Array.from({
+        length: Math.ceil(allItems.length / itemsPerPage),
+      }).map((_, index) => (
+        <button key={index} onClick={() => paginate(index + 1)}>
+          {index + 1}
+        </button>
+      ))}
     </div>
   );
 }
